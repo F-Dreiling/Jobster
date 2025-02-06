a05079d789e129c151a4d075b7313fa0d36c2f85
FX Drooklyn <dr.f380@hotmail.com>
2025-02-06 18:20:12 +0100

    models/views

---
+++ resources/views/listings.blade.php
@@ -0,0 +1,21 @@
+<h1>Jobster</h1>
+<h2>
+    {{ $heading }}
+</h2>
+
+@unless (count($listings) == 0)
+
+    @foreach ($listings as $listing)
+        <h3>
+            <a href="/listings/{{ $listing['id'] }}">{{ $listing['title'] }}</a>
+        </h3>
+        <p>
+            {{ $listing['description'] }}
+        </p>
+    @endforeach
+
+@else
+
+    <p>No listings found</p>
+
+@endunless
--- resources/views/listings.php
+++
@@ -1,8 +0,0 @@
-<h1>Jobster</h1>
-<h2><?php echo $heading; ?></h2>
-<?php 
-    foreach($listings as $listing) {
-        echo "<h3>" . $listing['title'] . "</h3>";
-        echo "<p>" . $listing['description'] . "</p>";
-    }
-?>
