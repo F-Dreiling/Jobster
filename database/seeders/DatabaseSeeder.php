3a598df517754dea467733a8c1e65ae3141c8388
FX Drooklyn <dr.f380@hotmail.com>
2025-02-07 13:42:27 +0100

    seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -36,5 +36,25 @@ class DatabaseSeeder extends Seeder
             'website' => 'https://www.starkindustries.com',
             'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam minima et illo reprehenderit quas possimus voluptas repudiandae cum expedita, eveniet aliquid, quam illum quaerat consequatur! Expedita ab consectetur tenetur delensiti?'
         ]);
+
+        Listing::create([
+            'title' => 'Laravel Developer', 
+            'tags' => 'laravel, vue, javascript',
+            'company' => 'Wayne Enterprises',
+            'location' => 'Gotham, NY',
+            'email' => 'email3@email.com',
+            'website' => 'https://www.wayneenterprises.com',
+            'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam minima et illo reprehenderit quas possimus voluptas repudiandae cum expedita, eveniet aliquid, quam illum quaerat consequatur! Expedita ab consectetur tenetur delensiti?'
+          ]);
+
+        Listing::create([
+            'title' => 'Backend Developer', 
+            'tags' => 'laravel, php, api',
+            'company' => 'Skynet Systems',
+            'location' => 'Newark, NJ',
+            'email' => 'email4@email.com',
+            'website' => 'https://www.skynet.com',
+            'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam minima et illo reprehenderit quas possimus voluptas repudiandae cum expedita, eveniet aliquid, quam illum quaerat consequatur! Expedita ab consectetur tenetur delensiti?'
+          ]);
     }
 }
