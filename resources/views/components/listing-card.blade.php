b5c479231d3da190cce2dc27a999a411a4e7f6b4
FX Drooklyn <dr.f380@hotmail.com>
2025-02-10 17:30:29 +0100

    refactor

--- resources/views/components/listing-card.blade.php
+++ resources/views/components/listing-card.blade.php
@@ -11,7 +11,9 @@
             <h3 class="text-2xl">
                 <a href="/listings/{{ $listing->id }}">{{ $listing->title }}</a>
             </h3>
+
             <div class="text-xl font-bold mb-4">{{ $listing->company }}</div>
+
             <ul class="flex">
                 <li class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs">
                     <a href="#">Laravel</a>
@@ -26,6 +28,7 @@
                     <a href="#">Vue</a>
                 </li>
             </ul>
+            
             <div class="text-lg mt-4">
                 <i class="fa-solid fa-location-dot"></i> 
                 {{ $listing->location }}
