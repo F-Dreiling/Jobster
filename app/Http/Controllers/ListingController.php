f17d9d9a5c8ec28a9746d759529934f3ab77d23d
FX Drooklyn <dr.f380@hotmail.com>
2025-02-12 11:31:08 +0100

    first build & refactoring, controller

---
+++ app/Http/Controllers/ListingController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Listing;
+use Illuminate\Http\Request;
+
+class ListingController extends Controller
+{
+    // index: show all listings
+    // show: show single listing
+    // create: show form to create new listing
+    // store: store new listing
+    // edit: show form to update listing
+    // update: update listing
+    // destroy: delete listing
+    
+    // Show all listings
+    public function index() {
+        return view('listings.index', [
+            'listings' => Listing::all()
+        ]);
+    }
+
+    // Show single listing
+    public function show(Listing $listing) {
+        return view('listings.show', [
+            'listing' => $listing
+        ]);
+    }
+}
