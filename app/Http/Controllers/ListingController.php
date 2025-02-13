6391ad4ddc4cc3a8ce39e185947dbef0709268b6
FX Drooklyn <dr.f380@hotmail.com>
2025-02-13 11:18:14 +0100

    mass assignment fillable & unguard

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -43,7 +43,8 @@ class ListingController extends Controller
             'location' => 'required',
             'email' => ['required', 'email'],
             'website' => 'required',
-            'tags' => 'required'
+            'tags' => 'required',
+            'description' => 'required'
         ]);
 
         Listing::create($formFields);
