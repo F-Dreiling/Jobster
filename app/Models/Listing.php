280111b6d531a4bb63e7b9d537e7240bfd1195ce
FX Drooklyn <dr.f380@hotmail.com>
2025-02-20 17:37:01 +0100

    ownership at creation

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Listing extends Model
 {
     use HasFactory;
-    protected $fillable = ['company', 'title', 'location', 'email', 'website', 'tags', 'description', 'logo'];
+    protected $fillable = ['user_id', 'company', 'title', 'location', 'email', 'website', 'tags', 'description', 'logo'];
 
     public function scopeFilter($query, array $filters) {
         // isset($filters['tag']) ? $filters['tag'] : false;
